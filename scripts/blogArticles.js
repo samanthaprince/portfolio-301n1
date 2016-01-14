7f12901a8a8818266bd757291316136b8db4d298
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-01-13 17:56:54 -0800

    commit linting changes

--- scripts/blogArticles.js
+++ scripts/blogArticles.js
@@ -3,7 +3,7 @@ var rawData = [
     title:      'Day One, Assignment One',
     category:   'Career Change 101',
     author:     'Samantha Prince',
-    authorUrl:   '',
+    //authorUrl:   '',
     publishedOn: '2016-01-04',
     body:         '<p>The first day of class was hard and I felt like I was drowning before i even started.</p>'
   },
@@ -11,7 +11,7 @@ var rawData = [
     title:      'Maybe things are looking Up?',
     category:   'Career Change 101',
     author:     'Samantha Prince',
-    authorUrl:   '',
+    //authorUrl:   '',
     publishedOn: '2016-01-06',
     body:         '<p>The second day of class started to get better, until we used Git/Github.</p>'
   },
