cd215edc5b7f3796708f68be050918a47aa104af
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-01-17 13:18:48 -0800

    add more info to blog body

--- scripts/blogArticles.js
+++ scripts/blogArticles.js
@@ -5,7 +5,7 @@ var rawData = [
     author:     'Samantha Prince',
     //authorUrl:   '',
     publishedOn: '2016-01-04',
-    body:         '<p>The first day of class was hard and I felt like I was drowning before i even started.</p>'
+    body:         '<p>The first day of class was hard and I felt like I was drowning before I even started. You can see what our first pair assignment looked like</p>'
   },
   {
     title:      'Maybe things are looking Up?',
@@ -13,6 +13,6 @@ var rawData = [
     author:     'Samantha Prince',
     //authorUrl:   '',
     publishedOn: '2016-01-06',
-    body:         '<p>The second day of class started to get better, until we used Git/Github.</p>'
+    body:         '<p>The second day of class started to get better, until we used Git/Github. Click the link to see our second pair assignment.</p>'
   },
 ];
