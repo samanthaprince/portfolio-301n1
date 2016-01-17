ef522d14e5879242e8ae8f5bfbffd7c758aadee6
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-01-17 12:58:30 -0800

    Added remaning js

--- scripts/article.js
+++ scripts/article.js
@@ -12,14 +12,32 @@ function Article(opts) {
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
 
+  $newArticle.attr('data-category', this.category);
+
   $newArticle.find('h1').html(this.title);
   $newArticle.find('.byline a').html(this.author);
   $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
   $newArticle.find('article-body').html(this.body);
 
   $newArticle.find('time[pubdate]').attr('title',this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
-  $newArticle.find('time').html('about' + parseInt(new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago';
+  $newArticle.append('<hr>');
 
+  $newArticle.removeClass('template');
   return $newArticle;
 };
+
+rawData.sort(function(a,b){
+  return(new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(ele){
+  articles.push(new Article(ele));
+});
+
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml());
+
+});
