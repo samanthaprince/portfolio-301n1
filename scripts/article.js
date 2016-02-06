784f8c72ec45b50b7585440af43b925d1704e11e
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-02-06 11:01:55 -0800

    change handlebars template to use correct variable

--- scripts/article.js
+++ scripts/article.js
@@ -10,27 +10,13 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.removeClass('template');
-  if (!this.publishedOn) {
-    $newArticle.addClass('draft');
-  }
+  var $newArticle = $('#article-template').html();
+  var compileTemplate = Handlebars.compile($newArticle);
 
-  $newArticle.attr('data-category', this.category);
-  $newArticle.attr('data-author', this.author);
-
-  $newArticle.find('h1').html(this.title);
-  $newArticle.find('.byline a').html(this.author);
-  $newArticle.find('.byline a').attr('href', this.authorUrl);
-  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
-  $newArticle.find('.article-body').html(this.body);
-
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
-  $newArticle.append('<hr>');
-
-  return $newArticle; 
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishedStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  var html = compileTemplate(this);
+  $('#articles').append(html);
 };
 
 rawData.sort(function(a,b) {
